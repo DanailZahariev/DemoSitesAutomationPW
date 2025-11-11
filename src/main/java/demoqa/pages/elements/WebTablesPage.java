bab1aff73206f85534d57113012a76033eee2c4a
danailzahariev <d.zahariev@icloud.com>
2025-11-11 19:23:59 +0200

    Updated WebTablesPage POM and WebTableTests for new tests

--- src/main/java/demoqa/pages/elements/WebTablesPage.java
+++ src/main/java/demoqa/pages/elements/WebTablesPage.java
@@ -6,30 +6,127 @@ import com.microsoft.playwright.Page;
 
 public class WebTablesPage extends BasePage {
 
-    private final Locator registrationAgeField;
+    private final Locator ageField;
     private final Locator submitButton;
+    private final Locator dataRows;
+    private final Locator addRow;
+    private final Locator firstNameField;
+    private final Locator lastNameField;
+    private final Locator emailField;
+    private final Locator salaryField;
+    private final Locator departmentField;
+
 
     public WebTablesPage(Page page) {
         super(page);
-        this.registrationAgeField = page.locator("#age");
+        this.ageField = page.locator("#age");
         this.submitButton = page.locator("#submit");
+        this.dataRows = page.locator(".rt-tbody .rt-tr-group .rt-tr:not(.-padRow)");
+        this.addRow = page.locator("#addNewRecordButton");
+        this.firstNameField = page.locator("#firstName");
+        this.lastNameField = page.locator("#lastName");
+        this.emailField = page.locator("#userEmail");
+        this.salaryField = page.locator("#salary");
+        this.departmentField = page.locator("#department");
+    }
+
+    public void setFirstNameField(String firstName) {
+        fill(firstNameField, firstName);
+    }
+
+    public void setLastNameField(String lastName) {
+        fill(lastNameField, lastName);
+    }
+
+    public void setEmailField(String email) {
+        fill(emailField, email);
+    }
+
+    public void setSalaryField(String salary) {
+        fill(salaryField, salary);
+    }
+
+    public void setDepartmentField(String department) {
+        fill(departmentField, department);
     }
 
     public void clickEdit(String email) {
-        String editLocator = "//div[text()='" + email + "']//following::span[@title='Edit'][1]";
-        this.page.locator(editLocator).click();
+        Locator row = getRowByUniqueText(email);
+        Locator editBtn = row.locator("//span[@title='Edit']");
+        click(editBtn);
+    }
+
+    public void deleteRow(String text) {
+        Locator row = getRowByUniqueText(text);
+        Locator deleteBtn = row.locator("//span[@title='Delete']");
+        click(deleteBtn);
     }
 
     public void setAge(String age) {
-        fill(registrationAgeField, age);
+        fill(ageField, age);
+    }
+
+    public String getRowFirstName(String text) {
+        Locator row = getRowByUniqueText(text);
+        Locator firstNameCell = row.locator(" .rt-td").nth(0);
+        return getText(firstNameCell);
+    }
+
+    public String getRowLastName(String text) {
+        Locator row = getRowByUniqueText(text);
+        Locator lastNameCell = row.locator(" .rt-td").nth(1);
+        return getText(lastNameCell);
+    }
+
+    public String getRowAge(String text) {
+        Locator row = getRowByUniqueText(text);
+        Locator ageCell = row.locator(" .rt-td").nth(2);
+        return getText(ageCell);
+    }
+
+    public String getRowEmail(String text) {
+        Locator row = getRowByUniqueText(text);
+        Locator emailCell = row.locator(" .rt-td").nth(3);
+        return getText(emailCell);
+    }
+
+    public String getRowSalary(String text) {
+        Locator row = getRowByUniqueText(text);
+        Locator salaryCell = row.locator(" .rt-td").nth(4);
+        return getText(salaryCell);
+    }
+
+    public String getRowDepartment(String text) {
+        Locator row = getRowByUniqueText(text);
+        Locator departmentCell = row.locator(" .rt-td").nth(5);
+        return getText(departmentCell);
+    }
+
+    public int getDataRows() {
+        return dataRows.count();
+    }
+
+    private Locator getRowByUniqueText(String uniqueText) {
+        return this.page.locator(".rt-tr-group")
+                .filter(new Locator.FilterOptions().setHasText(uniqueText));
+    }
+
+    public void addNewUser(String firstName, String lastName, String email, String age, String salary, String department) {
+        clickAddNewRow();
+        setFirstNameField(firstName);
+        setLastNameField(lastName);
+        setEmailField(email);
+        setAge(age);
+        setSalaryField(salary);
+        setDepartmentField(department);
+        clickSubmit();
+    }
+
+    public void clickAddNewRow() {
+        click(addRow);
     }
 
     public void clickSubmit() {
         click(submitButton);
-    }
-
-    public String getTableAge(String email) {
-        String tableAgeLocator = "//div[normalize-space()='" + email + "']/preceding::div[1]";
-        return this.page.locator(tableAgeLocator).textContent();
     }
 }
--- src/test/java/demoqa/elements/WebTableTests.java
+++ src/test/java/demoqa/elements/WebTableTests.java
@@ -17,10 +17,55 @@ public class WebTableTests extends BaseTest {
         webTablesPage.setAge(age);
         webTablesPage.clickSubmit();
 
-        String actualAge = webTablesPage.getTableAge(email);
+        String actualAge = webTablesPage.getRowAge(email);
 
         Assert.assertEquals(actualAge, expectedAge, "Age is not correct");
 
     }
 
+    public void testWebTableDelete() {
+        String email = "cierra@example.com";
+        int expectedRows = 3;
+        var webTablePage = homePage.goToElements().clickWebTables();
+        int dataRowCount = webTablePage.getDataRows();
+
+        Assert.assertEquals(dataRowCount, expectedRows, "Table has incorrect number of rows");
+
+        webTablePage.deleteRow(email);
+        dataRowCount = webTablePage.getDataRows();
+        expectedRows = 2;
+
+        Assert.assertEquals(dataRowCount, expectedRows, "Row wasn't deleted" + "Expected rows: 2" + "Actual rows: " + dataRowCount);
+    }
+
+    public void testAddNewRow() {
+        var firstName = "Ivan";
+        var lastName = "Ivanov";
+        var email = "ivan@example.com";
+        var age = 20;
+        var salary = 5000;
+        var department = "QA";
+        int expectedRows = 4;
+
+        var webTablePage = homePage.goToElements().clickWebTables();
+        webTablePage.addNewUser(firstName, lastName, email, String.valueOf(age), String.valueOf(salary), department);
+
+        int dataRowCount = webTablePage.getDataRows();
+
+        Assert.assertEquals(dataRowCount, expectedRows, "Row wasn't added");
+
+        String actualFirstName = webTablePage.getRowFirstName(email);
+        String actualLastName = webTablePage.getRowLastName(email);
+        String actualAge = webTablePage.getRowAge(email);
+        String actualEmail = webTablePage.getRowEmail(email);
+        String actualSalary = webTablePage.getRowSalary(email);
+        String actualDepartment = webTablePage.getRowDepartment(email);
+
+        Assert.assertEquals(String.valueOf(age), actualAge);
+        Assert.assertEquals(String.valueOf(salary), actualSalary);
+        Assert.assertEquals(actualFirstName, firstName);
+        Assert.assertEquals(actualLastName, lastName);
+        Assert.assertEquals(actualEmail, email);
+        Assert.assertEquals(actualDepartment, department);
+    }
 }
