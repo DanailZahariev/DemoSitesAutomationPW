fa61001d16a10a3540a23d1c9915bc31a83b2500
danailzahariev <d.zahariev@icloud.com>
2025-11-12 13:13:45 +0200

    Updated POMs and tests to use playwright assertion

--- src/test/java/demoqa/frames/FrameTests.java
+++ src/test/java/demoqa/frames/FrameTests.java
@@ -1,34 +1,32 @@
 package demoqa.frames;
 
 import demoqa.base.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @Test(suiteName = "Frame Tests")
 public class FrameTests extends BaseTest {
 
     public void testFramesBigBox() {
         var framesPage = homePage.goToAlertFrameWindow().clickFrames();
-        String actualText = framesPage.getTextInBigFrame();
         String expectedText = "This is a sample page";
 
-        Assert.assertEquals(actualText, expectedText, "\n Actual & expected text are not equal\n");
-        String actualHeaderText = framesPage.getHeaderFramesText();
+        assertThat(framesPage.getTextInBigFrame()).hasText(expectedText);
+
         String expectedHeaderText = "Frames";
 
-        Assert.assertEquals(actualHeaderText, expectedHeaderText, "\n Actual & expected text are not equal\n");
+        assertThat(framesPage.getHeaderFramesText()).hasText(expectedHeaderText);
     }
 
     public void testFramesSmallBox() {
         var framesPage = homePage.goToAlertFrameWindow().clickFrames();
-        String actualText = framesPage.getTextInSmallFrame();
         String expectedText = "This is a sample page";
 
-        Assert.assertEquals(actualText, expectedText, "\n Actual & expected text are not equal\n");
+        assertThat(framesPage.getTextInSmallFrame()).hasText(expectedText);
 
-        String actualHeaderText = framesPage.getHeaderFramesText();
         String expectedHeaderText = "Frames";
 
-        Assert.assertEquals(actualHeaderText, expectedHeaderText, "\n Actual & expected text are not equal\n");
+        assertThat(framesPage.getHeaderFramesText()).hasText(expectedHeaderText);
     }
 }
