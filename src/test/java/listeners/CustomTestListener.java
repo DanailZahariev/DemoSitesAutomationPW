8bb6f6feb266941c41d124b22a28acc312e8102c
danailzahariev <d.zahariev@icloud.com>
2025-11-14 16:21:35 +0200

    Added CustomTestListener class

--- src/test/java/listeners/CustomTestListener.java
+++ src/test/java/listeners/CustomTestListener.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.Reporter;
+import utilities.ScreenshotHelper;
+
+import java.io.File;
 
 public class CustomTestListener implements ITestListener {
 
@@ -22,6 +26,28 @@ public class CustomTestListener implements ITestListener {
     @Override
     public void onTestFailure(ITestResult result) {
         failCount++;
+        LOGGER.error("❌ FAILED: {}", result.getName());
+
+        String screenshotPath = ScreenshotHelper.captureScreenshot(result.getName());
+
+        if (screenshotPath != null) {
+            File screenshot = new File(screenshotPath);
+            if (screenshot.exists()) {
+                String relativePath = screenshotPath.replace("\\", "/");
+
+                Reporter.log("<br><b>Screenshot:</b><br>");
+                Reporter.log("<a href='../" + relativePath + "' target='_blank'>");
+                Reporter.log("<img src='../" + relativePath + "' height='400' width='600' style='border:2px solid red;'/>");
+                Reporter.log("</a><br>");
+
+                LOGGER.info("\uD83D\uDCF8 Screenshot attached: {}", screenshotPath);
+            }
+        }
+
+        if (result.getThrowable() != null) {
+            Reporter.log("<br><b>Error:</b><br>");
+            Reporter.log("<pre>" + result.getThrowable().getMessage() + "</pre>");
+        }
     }
 
     @Override
