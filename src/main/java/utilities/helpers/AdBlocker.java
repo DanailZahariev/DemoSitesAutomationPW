d50e7c88bdb4654472f11a4d12bdc49c97e6e32f
danailzahariev <d.zahariev@icloud.com>
2025-11-19 10:38:50 +0200

    minor changes

--- src/main/java/utilities/helpers/AdBlocker.java
+++ src/main/java/utilities/helpers/AdBlocker.java
@@ -5,31 +5,36 @@ import com.microsoft.playwright.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 public class AdBlocker {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdBlocker.class);
-    private static final List<String> AD_PATTERNS = loadAdPatterns();
+    private static final List<String> AD_PATTERNS = List.of(
+            "doubleclick",
+            "googlesyndication",
+            "googleadservices",
+            "google-analytics",
+            "googletagmanager",
+            "googletagservices",
+            "adservice",
+            "/ads/",
+            "/ad/",
+            "_ads",
+            "analytics",
+            "tracking",
+            "pagead",
+            "adserver",
+            "advertisement"
+    );
+
+    private static final List<String> IMAGE_AD_PATTERNS = List.of(
+            "ads",
+            "banner",
+            "sponsor"
+    );
 
     private AdBlocker() {
-    }
-
-    private static List<String> loadAdPatterns() {
-        try (var inputStream = AdBlocker.class.getClassLoader().getResourceAsStream("adblock-patterns.txt")) {
-            if (inputStream == null) {
-                LOGGER.warn("Ad patterns file not found!");
-                return List.of();
-            }
-            return new String(inputStream.readAllBytes()).lines().toList();
-        } catch (IOException e) {
-            LOGGER.warn("Could not load ad patterns, using defaults", e);
-            return List.of();
-        }
     }
 
     public static void blockAdsAndTracking(BrowserContext context) {
@@ -37,10 +42,14 @@ public class AdBlocker {
             Request request = route.request();
             String requestUrl = request.url().toLowerCase();
             String resourceType = request.resourceType();
-            if (AD_PATTERNS.stream().anyMatch(requestUrl::contains)) {
-                route.abort();
-                LOGGER.info("Blocked ad request: {}", route.request().url());
-            } else if (AD_PATTERNS.stream().anyMatch(resourceType::contains)) {
+
+            boolean isGeneralAd = AD_PATTERNS.stream().anyMatch(requestUrl::contains);
+
+            boolean isImageAd = resourceType.equals("image") &&
+                    IMAGE_AD_PATTERNS.stream().anyMatch(requestUrl::contains);
+
+            if (isGeneralAd || isImageAd) {
+                LOGGER.debug("Blocked ad request: {}", requestUrl);
                 route.abort();
             } else {
                 route.resume();
